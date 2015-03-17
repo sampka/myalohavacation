b04773a1c39364fea08e3aeb0478fc104f992220
sampka <sampka@gmail.com>
2015-03-17 15:46:40 -0400

    Final

--- 2/send.php
+++ 2/send.php
@@ -1,5 +1,38 @@
 <?php
 //--------------------------Set these paramaters--------------------------
+$Condo = $_POST['Condo'];
+$guests = $_POST['guests'];
+$payment = $_POST['payment'];
+$AriveMonth = $_POST['AriveMonth'];
+$AriveDay = $_POST['AriveDay'];
+$AriveYear = $_POST['AriveYear'];
+$DepartMonth = $_POST['DepartMonth'];
+$DepartDay = $_POST['DepartDay'];
+$DepartYear = $_POST['DepartYear'];
+$PAddress = $_POST['PAddress'];
+$hPhone = $_POST['hPhone'];
+$hPhone2 = $_POST['hPhone2'];
+$hPhone4 = $_POST['hPhone4'];
+$cPhone = $_POST['cPhone'];
+$cPhone2 = $_POST['cPhone2'];
+$cPhone3 = $_POST['cPhone3'];
+$employ =  $_POST['employ'];
+$employaddress =  $_POST['employaddress'];
+$ContactPandP =  $_POST['ContactPand'];
+$conPhone =  $_POST['conPhone'];
+$conPhone3 =  $_POST['conPhone3'];
+$conPhone4 =  $_POST['conPhone4'];
+$EContactname =  $_POST['EContactname'];
+$EContactAddress =  $_POST['EContactAddress'];
+$ePhone1 =  $_POST['ePhone1'];
+$ePhone2 =  $_POST['ePhone2'];
+$ePhone3 =  $_POST['ePhone3'];
+$_parking =  $_POST['_parking'];
+$DueMonth =  $_POST['DueMonth'];
+$DueDay =  $_POST['DueDay'];
+$DueYear =  $_POST['DueYear'];
+$Additional =  $_POST['Additional'];
+
 
 // Subject of email sent to you.
 $subject = 'Application for '.clean_string($Condo)."  Guests: ".clean_string($guests); 
@@ -8,7 +41,7 @@ $subject = 'Application for '.clean_string($Condo)."  Guests: ".clean_string($gu
 $emailadd = 'elaine@myalohavacation.com'; 
 
 // Where to redirect after form is processed. 
-$url = 'http://www.myalohavacation.com/1/application.html'; 
+$urlreturn = 'http://www.myalohavacation.com/2/application.html'; 
 
 function died($error) {
 		// your error code can go here
@@ -23,7 +56,7 @@ function died($error) {
 		died('We are sorry, but there appears to be a problem with the form your submitted.');		
 	}
 
-   
+   <F5>
 	$email = $_POST['email']; // required
 	
 	
@@ -64,8 +97,140 @@ function clean_string($string) {
     $email_message .= "Additional Comments or Questions: ".clean_string($Additional)."\n\n";
 
 
-#printf($email_message);
+	#printf($email_message);
+	
 
-mail($emailadd, $subject, $email_message);
-echo '<META HTTP-EQUIV=Refresh CONTENT="0; URL='.$url.'">';
+
+ print_r($response);
+	
+
+#mail($emailadd, $subject, $email_message);
+
+
+
+ $url = 'https://api.sendgrid.com/';
+ $user = 'azure_85cc94cc954ae67d5f6ffe0beb837869@azure.com';
+ $pass = '5tizzoB7HGrK6P7'; 
+
+
+ $params = array(
+      'api_user' => $user,
+      'api_key' => $pass,
+      'to' => $emailadd,
+      'subject' =>  $subject,
+      'html' => $email_message,
+      'text' => $email_message,
+      'from' => 'application@myalohavacation.com',
+   );
+
+ $request = $url.'api/mail.send.json';
+
+
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+        $human = intval($_POST['human']);
+        $from = "From: Contact Form";
+        $mobile = $_POST['number'];
+
+        $to = $emailadd; 
+    
+
+       
+
+        $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+ 
+  
+        $humanBool=66;
+
+        $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+        // now we go through some validation of the parts in the form 
+        // to check everything was entered. In hindsight HTML 5 
+        // 'required' attribute is much easier and fulfills exactly 
+        // what I did here anyway.
+        // Check if name has been entered
+     
+
+        // Check if email has been entered and is valid
+       
+
+        //Check if message has been entered
+       
+          $humanBool = 66;
+
+     
+
+        // If there are no errors in the data fields i.e. missing data
+        
+                $url = 'https://api.sendgrid.com/';
+                //create array params for api call
+                //these params are what appear in the email that arrives into your email account.
+              $params = array(
+      'api_user' => $user,
+      'api_key' => $pass,
+      'to' => $emailadd,
+      'subject' =>  $subject,
+      'html' => $email_message,
+      'text' => $email_message,
+      'from' => 'application@myalohavacation.com',
+   );
+
+                // I don't why I concatenated this but one of the 
+                // resources I used while researching said to do it. It 
+                // worked, it's also unneccessary. $request is just 
+                // https://api.sendgrid.com/api/mail.send.json. I think 
+                // the founder of that article is just having a private 
+                // joke at people using his code for help.
+
+                //concatenate api url to url above
+                $request =  $url.'api/mail.send.json';
+
+                //debugging
+                //$error = error_get_last();
+                //echo this to see what errors are happening in the file
+
+                // Repetitive code.....
+                $url2 = 'https://api.sendgrid.com/api/mail.send.json';
+
+
+                // Okay queue magic time. I'll explain it as overview
+                // here and you guys can step through after the 
+                // explanation. 1) magic. 2) Sorcery. I don't quite get 
+                // all of it so my explanation would be poor but I
+                // will say HOW it works overall. All previous arrays
+                // and variables are packaged up in one pack and then
+                // a service is called and they are sent as $result 
+
+
+
+                // use key 'http' even if you send the request to https://
+                $options = array(
+                    'http' => array(
+                        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                        'method'  => 'POST',
+                        'content' => http_build_query($params),
+                    ),
+                );
+                $context  = stream_context_create($options);
+                $result = file_get_contents($url2, false, $context);
+
+                // debugging code if something goes wrong 
+                // var_dump($result);
+                $result='<div class="alert alert-success">Thank You! I will be in touch</div>';
+
+                // this is here to reset the page and clear the fields
+                // of the form once mail has been sent.
+                $page = $_SERVER['PHP_SELF'];
+                $sec = "3";
+                header("Refresh: $sec; url=$urlreturn");
+
+
+		echo '<META HTTP-EQUIV=Refresh CONTENT="0; URL='.$urlreturn.'">'
+
 ?>
+
+
+
+
