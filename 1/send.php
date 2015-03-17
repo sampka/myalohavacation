5644c202e52bc6dc62d579e69d8097b966b39578
sampka <sampka@gmail.com>
2015-03-17 13:06:58 -0400

    Revert "asd"
    This reverts commit f77024d1deb4b01572dce8257636100a54c0d071.
    
    Conflicts:
    	1/send.php
    	1/send.php~

--- 1/send.php
+++ 1/send.php
@@ -31,12 +31,32 @@
         $to = 'thisiswhereitsgoing@gmail.com'; 
         $subject = 'Message for subject line of email';
 
+<<<<<<< HEAD
        
 
         $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
  
   
+=======
+        $humanBool=66;
+
+        $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+        // now we go through some validation of the parts in the form 
+        // to check everything was entered. In hindsight HTML 5 
+        // 'required' attribute is much easier and fulfills exactly 
+        // what I did here anyway.
+        // Check if name has been entered
+     
+
+        // Check if email has been entered and is valid
+       
+
+        //Check if message has been entered
+       
+          $humanBool = 66;
+>>>>>>> parent of f77024d... asd
      
 
         // If there are no errors in the data fields i.e. missing data
@@ -48,8 +68,13 @@
                     'api_user'  => $user,
                     'api_key'   => $pass,
                     'to'        => 'sampka@gmail.com',
+<<<<<<< HEAD
                     'subject'   => 'subject',
                     'html'      => 'newtest',
+=======
+                    'subject'   => 'Subject',
+                    'html'      => 'test',
+>>>>>>> parent of f77024d... asd
                     'text'      => 'this is the text element',
                     'from'      => 'test@myalohavacation.com',
                   );
@@ -102,8 +127,24 @@
                 $page = $_SERVER['PHP_SELF'];
                 $sec = "3";
                 header("Refresh: $sec; url=$page");
+<<<<<<< HEAD
 
 
 
 
 ?>
+=======
+
+
+
+?>
+// after this goes the HTML form here is one box from the form as its 
+// all the same no need to repeat it all I think.
+
+<div class="form-group">
+
+                        <div class="col-xs-10 col-xs-offset-1">
+                            <input type="text" class="form-control" id="name" name="name" placeholder="First & Last Name" style="text-transform:capitalize" value="<?php echo htmlspecialchars($_POST['name']); ?>" required>
+                            <?php echo "<p class='text-danger'>$errName</p>";?>
+                        </div>
+>>>>>>> parent of f77024d... asd
